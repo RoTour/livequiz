4410477a35a84a2f2598beb56805a37fc45713f2
Rotour <rotour.dev@gmail.com>
2026-01-28 16:25:19 +0100

    update: backend test execution time: run  for unit, 

--- backend/src/test/java/com/livequiz/backend/domain/quiz/QuizTest.java
+++ backend/src/test/java/com/livequiz/backend/domain/quiz/QuizTest.java
@@ -3,7 +3,9 @@ package com.livequiz.backend.domain.quiz;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("memory")
 public class QuizTest {
 
   @Test
--- backend/src/test/java/com/livequiz/backend/infrastructure/web/QuizControllerIT.java
+++ backend/src/test/java/com/livequiz/backend/infrastructure/web/QuizControllerIT.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class QuizControllerTest {
+@ActiveProfiles("memory")
+public class QuizControllerIT {
 
   @Autowired
   private MockMvc mockMvc;
